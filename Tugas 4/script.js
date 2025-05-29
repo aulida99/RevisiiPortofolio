5baec8288dc238710c963a44cbeeabd493a1bec2
aulida99 <aulidaizza1@gmail.com>
2025-05-29 10:22:04 +0700

    Add files via upload

---
+++ Tugas 4/script.js
@@ -0,0 +1,8 @@
+//JavaScript for Toggle
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
